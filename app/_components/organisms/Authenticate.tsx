60f7086ec4118f9bfd47b2c6d10fdf666233ae01
BosEriko <bos.eriko@gmail.com>
2024-06-13 13:24:37 +0800

    update ui for authentication molecule

--- app/_components/organisms/Authenticate.tsx
+++ app/_components/organisms/Authenticate.tsx
@@ -1,8 +1,11 @@
 "use client";
 import { useState } from 'react';
 import { request } from 'graphql-request';
+import { Input } from 'antd';
 import SignInTenantMutation from "../../_definitions/mutations/tenant/sign-in";
 import CreateTenantMutation from "../../_definitions/mutations/tenant/create";
+
+import Atom from "@atom";
 
 const Authenticate: React.FunctionComponent = () => {
   const [identifier, setIdentifier] = useState('');
@@ -26,26 +29,31 @@ const Authenticate: React.FunctionComponent = () => {
   };
 
   return (
-    <div>
-      <div>
-        <button onClick={() => setIsSignIn(!isSignIn)}>
-          {!isSignIn ? "Sign In" : "Register"}
-        </button>
-      </div>
-      <div>
-        <input
+    <div className="bg-[#cd3c2b] flex flex-col gap-5 justify-center items-center w-screen h-screen">
+      <Atom.Logo width={100} height={100} />
+      <div className="w-full max-w-80 bg-white rounded shadow p-5 flex flex-col gap-3">
+        <div className="text-center text-2xl text-gray-600 mb-4">{isSignIn ? "Sign In" : "Register"}</div>
+        <Atom.Input
           type="text"
           placeholder="Identifier"
           value={identifier}
           onChange={(e) => setIdentifier(e.target.value)}
         />
-        <input
+        <Input.Password
           type="password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button onClick={handleSubmit}>{isSignIn ? "Sign In" : "Register"}</button>
+        <div className="flex justify-between">
+          <Atom.Button type="link" className="hover:underline text-[#cd3c2b] p-0 m-0" onClick={null}>Forgot Password?</Atom.Button>
+          <Atom.Button onClick={handleSubmit}>{isSignIn ? "Sign In" : "Register"}</Atom.Button>
+        </div>
+      </div>
+      <div className="mb-40">
+        <Atom.Button type="link" className="text-white !hover:text-white hover:underline" onClick={() => setIsSignIn(!isSignIn)}>
+          {!isSignIn ? "Sign In" : "Register"}
+        </Atom.Button>
       </div>
     </div>
   );
