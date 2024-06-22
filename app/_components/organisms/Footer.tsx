20dbb1929b13595e150c6308424557a93c300fae
BosEriko <bos.eriko@gmail.com>
2024-06-22 13:51:22 +0800

    update footer UI

--- app/_components/organisms/Footer.tsx
+++ app/_components/organisms/Footer.tsx
@@ -2,19 +2,22 @@ import { HeartIcon } from "@heroicons/react/solid";
 import Atom from "@atom";
 
 const Footer: React.FunctionComponent = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+
   return (
-    <footer className="shadow-lg">
-      <div className="bg-gray-600 text-white">
+    <footer>
+      <div className="bg-gray-200 text-black">
         <div className="container mx-auto py-3 flex flex-col items-center gap-1 text-sm">
-          <div>Kuru Studio Panel &copy; 2023</div>
+          <div>Kuru Studio Panel &copy; {year}</div>
         </div>
       </div>
-      <div className="bg-gray-800 text-white">
+      <div className="bg-gray-300 text-black">
         <div className="container mx-auto py-3 flex flex-col items-center gap-1 text-sm">
           <div className="flex items-center gap-1">
             Made with <HeartIcon className="h-5 w-5 text-red-500" /> by
           </div>
-          <div>Kuru Studio Team and the Kuru Studio Community</div>
+          <div><span className="font-bold">Kuru Studio Team</span> and the <span className="font-bold">Kuru Studio Community</span></div>
           <div className="mt-2">
             <Atom.GitHub />
           </div>
