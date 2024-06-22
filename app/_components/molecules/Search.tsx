588860451c8e547e9b3f2af8beab46d76724bd87
BosEriko <bos.eriko@gmail.com>
2024-06-22 14:30:52 +0800

    create search component

---
+++ app/_components/molecules/Search.tsx
@@ -0,0 +1,5 @@
+const Search = () => {
+  return <div className="font-bold">Search</div>;
+};
+
+export default Search;
--- app/_components/molecules/index.ts
+++ app/_components/molecules/index.ts
@@ -1,7 +1,9 @@
 import NavigationLink from "./NavigationLink";
+import Search from "./Search";
 
 const Molecule = {
   NavigationLink,
+  Search,
 }
 
 export default Molecule;
