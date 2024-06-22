16f7d843293f6487d478726633104172658c58ff
BosEriko <bos.eriko@gmail.com>
2024-06-22 10:31:52 +0800

    fix API_PATH env not appearing

--- app/_utilities/env.ts
+++ app/_utilities/env.ts
@@ -1,6 +1,7 @@
-// Note: Declared ENV variables must start with "NEXT_" to work
+// Note: Declared ENV variables must start with "NEXT_PUBLIC_" to work in the browser
+// More Information: https://nextjs.org/docs/pages/building-your-application/configuring/environment-variables#bundling-environment-variables-for-the-browser
 const env = {
-  apiPath: process.env.NEXT_API_PATH,
+  apiPath: process.env.NEXT_PUBLIC_API_PATH,
 }
 
 export default env;
