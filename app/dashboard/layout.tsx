c232400b906d994052275e4d5bc8a56290809daa
BosEriko <bos.eriko@gmail.com>
2024-06-22 20:21:12 +0800

    create specific layout for pages and add dashboard page

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,13 @@
+import Template from "@template";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <>{children}</>
+    <Template.Default>
+      {children}
+    </Template.Default>
   )
 }
