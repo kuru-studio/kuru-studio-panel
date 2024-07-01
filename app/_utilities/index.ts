75b4155276f14fa58abd18356eee629a9a52ce1d
BosEriko <bos.eriko@gmail.com>
2024-07-01 21:41:05 +0800

    add utility for useBreakpoint

--- app/_utilities/index.ts
+++ app/_utilities/index.ts
@@ -1,4 +1,5 @@
 import camelCase from 'lodash/camelCase';
+import { useState, useEffect } from 'react';
 
 type Column = {
   title: string;
@@ -12,4 +13,35 @@ export const createColumnsForTable = (columns: string[]): Column[] => {
     dataIndex: camelCase(column),
     key: camelCase(column),
   }));
+};
+
+type Breakpoints = {
+  [key: string]: number;
+};
+
+export const breakpoints: Breakpoints = {
+  "sm": 640,
+  "md": 768,
+  "lg": 1024,
+  "xl": 1280,
+  "2xl": 1536,
+};
+
+export const useBreakpoint = (breakpoint: keyof typeof breakpoints): boolean => {
+  const [matches, setMatches] = useState(false);
+  const query = `(min-width: ${breakpoints[breakpoint]}px)`;
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(query);
+    setMatches(mediaQuery.matches);
+
+    const handler = (event) => setMatches(event.matches);
+    mediaQuery.addEventListener("change", handler);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handler);
+    };
+  }, [query]);
+
+  return matches;
 };
