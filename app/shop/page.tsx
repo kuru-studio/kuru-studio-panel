03443ddbf7a64c494a78c5a0645676227619ba4a
BosEriko <bos.eriko@gmail.com>
2024-07-01 21:48:13 +0800

    add example table for shop page

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,7 +1,31 @@
+"use client";
 import Organism from "@organism";
 import { ShopOutlined } from "@ant-design/icons";
+import { Table } from "antd";
+import { createColumnsForTable } from "../_utilities";
 
-export default async function ShopPage() {
+const data = [
+  {
+    key: '1',
+    name: 'Mike',
+    age: 32,
+    address: '10 Downing Street',
+  },
+  {
+    key: '2',
+    name: 'John',
+    age: 42,
+    address: '10 Downing Street',
+  },
+];
+
+const headers = createColumnsForTable([
+  "Name",
+  "Age",
+  "Address",
+]);
+
+export default function ShopPage() {
   return (
     <div>
       <Organism.Header
@@ -14,7 +38,7 @@ export default async function ShopPage() {
         ]}
       />
       <Organism.Content>
-        Content
+        <Table dataSource={data} columns={headers} />
       </Organism.Content>
     </div>
   );
